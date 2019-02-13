016a43aa2b482b6d9b63d9a07f4c624769b808fa
Seth Dandridge <sethdan@gmail.com>
2019-02-13 23:27:12 +0000

    Alerts formatting, dashboard/book skeleton

--- fortnite/dashboard.py
+++ fortnite/dashboard.py
@@ -6,7 +6,14 @@ from fortnite.db import get_db
 
 bp = Blueprint("dashboard", __name__)
 
+
 @bp.route("/")
 @login_required
 def index():
     return render_template("dashboard/index.jinja2")
+
+
+@bp.route("/book")
+@login_required
+def book(methods=("GET", "POST")):
+    return render_template("dashboard/book.jinja2")
