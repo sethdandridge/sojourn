871a8e37b5595a6af3477eef24ec6fbfda72cd4e
Seth Dandridge <sethdan@gmail.com>
2019-02-13 21:18:16 +0000

    Dashboard index, main container styling

--- fortnite/dashboard.py
+++ fortnite/dashboard.py
@@ -6,15 +6,7 @@ from fortnite.db import get_db
 
 bp = Blueprint("dashboard", __name__)
 
-
 @bp.route("/")
 @login_required
 def index():
-    db = get_db()
-    users = db.execute("SELECT * FROM user;").fetchall()
-    return_string = ""
-    for user in users:
-        for key in user.keys():
-            return_string += f"{key}: {user[key]} "
-        return_string += "<br />"
-    return return_string
+    return render_template("dashboard/index.jinja2")
